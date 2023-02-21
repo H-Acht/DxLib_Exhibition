1fc4dbb389e191c5883f1fcc0483230adcc9dcc2
橋垣 冴祐 <fko2247061@stu.o-hara.ac.jp>
2023-02-21 12:07:37 +0900

    マクロ

--- DxLib_Exhibition/game.h
+++ DxLib_Exhibition/game.h
@@ -1,17 +1,23 @@
 #pragma once
 #include "DxLib.h"
-#define ENEMY 8
+#define DIR 8
+#define ENEMY 3
 #define TORCH 4
 
 namespace Game
 {
+#ifdef _DEBUG //デバッグ
     //ウィンドウモード設定
     constexpr bool kWindowMode = true;
+#else
+    //ウィンドウモード設定
+    constexpr bool kWindowMode = false;
+#endif
     //ウィンドウ名設定
     const char* const kTitleText = "ゲーム名";
     //ウィンドウサイズ
-    constexpr int kScreenWidth = 800;
-    constexpr int kScreenHeight = 800;
+    constexpr int kScreenWidth = 1600;
+    constexpr int kScreenHeight = 900;
     //カラーモード
     constexpr int kColorDepth = 32;     //32 or 16
 };
